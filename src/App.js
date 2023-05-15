7d951b0bdd76d91883769504d830814bb211debd
Liliana <liliana.strajescu96@gmail.com>
2023-05-15 22:09:09 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./styles.css";
+import axios from "axios";
+
+export default function App() {
+  let [city, setCity] = useState("");
+  let [weather, setWeather] = useState(null);
+  let apiKey = "ed55b36e362d8733f7d859247cedeaf2";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(showWeather);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setWeather(city);
+  };
+
+  function showWeather(response) {
+    setWeather(response.data);
+    console.log(response.data);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="City"
+          value={city}
+          on={(e) => setCity(e.target.value)}
+        />
+        <input type="submit" />
+      </form>
+      <div>
+        {weather && (
+          <div>
+            <ul>
+              <li>Weather in {weather.name}</li>
+              <li>Temperature: {weather.main.temp}°C</li>
+              <li>Humidity: {weather.main.humidity}%</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <div className="App">
+      <h1>Weather App</h1>
+      <App />
+    </div>
+  </StrictMode>
+);
